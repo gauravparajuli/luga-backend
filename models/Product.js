61cdc8e10563bd1d279cad06a529d7350714b604
gaurav parajuli <gparajuli.1997@gmail.com>
2022-12-23 06:20:16 +0545

    completed product model

---
+++ models/Product.js
@@ -0,0 +1,35 @@
+import { Schema, model } from 'mongoose'
+
+const ProductSchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        image: {
+            type: String,
+            required: true,
+        },
+        categories: {
+            type: Array,
+        },
+        size: {
+            type: String,
+        },
+        color: {
+            type: String,
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+    },
+    { timestamps: true }
+)
+
+export default model('Product', ProductSchema)
